77a4e2e91160bba1b73d38bd2c927f8999ce14f5
unknown <aktarulmondal2740@gmail.com>
2022-07-08 10:35:32 -0700

    final commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+app.use(express.static(__dirname,+"./build"))
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./build/index.html"))
+})
+app.listen(4000,()=>{
+
+})
